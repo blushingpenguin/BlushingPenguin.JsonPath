176816fd96dde3d7e142bc41d2592a893bc5d517
blushingpenguin <mark@blushingpenguin.com>
2020-06-08 16:17:49 +0100

    remove debug code

--- BlushingPenguin.JsonPath/ScanFilter.cs
+++ BlushingPenguin.JsonPath/ScanFilter.cs
@@ -16,14 +16,8 @@ namespace BlushingPenguin.JsonPath
         {
             foreach (JsonElement c in current)
             {
-                // if (Name == null)
-                // {
-                //     yield return c;
-                // }
-
                 foreach (var e in GetScanValues(c))
                 {
-                    System.Diagnostics.Debug.WriteLine($"sv = {e}, me name = {Name}");
                     if (e.Name == Name)
                     {
                         yield return e.Value;
